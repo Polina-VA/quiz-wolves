54564af191646a85e647f056879506fd7a8f9fb6
Excomunicad0 <e_zhul@mail.ru>
2024-09-06 13:31:49 +0300

    feat:ResultRoutes

--- server/routes/question.routes.js
+++ server/routes/question.routes.js
@@ -10,28 +10,14 @@ questionRouter.get('/', async (req, res) => {
   }
 });
 
-questionRouter.get('/:theme_id', async (req, res) => {
+questionRouter.get('/:id', async (req, res) => {
   try {
-    const { theme_id } = req.params;
-    const questions = await QuestionService.getQuestionsByThemeId(theme_id);
-    res.status(200).json({ message: 'success', questions });
+    const { id } = req.params;
+    const question = await QuestionService.getQuestionId(id);
+    res.status(200).json({ message: 'success', question });
   } catch ({ message }) {
     res.status(500).json({ error: message });
   }
 });
-
-// questionRouter.get('/:theme_id/:id', async (req, res) => {
-//   try {
-//     const { theme_id, id } = req.params;
-//     const questions = await QuestionService.getAllQuestion({
-//       where: { theme_id, id },
-//     });
-//     res.status(200).json({ message: 'success', questions });
-//   } catch ({ message }) {
-//     res.status(500).json({ error: message });
-//   }
-// });
-
-// questionRouter.get('');
 
 module.exports = questionRouter;
--- server/services/Question.services.js
+++ server/services/Question.services.js
@@ -6,20 +6,9 @@ class QuestionService {
     (await Question.findAll()).map((question) => question.get());
 
   static getQuestionId = async (id) => {
-    const question = await Question.findByPk(id);
+    const question = await Question.findOne({ where: { id } });
     return question;
   };
-
-  static getQuestionsByThemeId = async (theme_id) => {
-    const questions = await Question.findAll({ where: { theme_id } });
-    return questions.map((question) => question.get());
-  };
-  
-  // static getFirstQuestionId = async (theme_id) => {
-  //   const allQuestion = await Question.findALl({ where: { theme_id } });
-  //   const firstQuestion = await allQuestion[0];
-  //   return firstQuestion;
-  // };
 }
 
 module.exports = QuestionService;
