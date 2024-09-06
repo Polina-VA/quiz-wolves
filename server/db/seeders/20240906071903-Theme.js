43a6b96740c253cd7e392fd47343df22b8dbbbc8
Excomunicad0 <e_zhul@mail.ru>
2024-09-06 10:41:12 +0300

    feat:createDB

---
+++ server/db/seeders/20240906071903-Theme.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Themes',
+      [
+        {
+          title: 'Тема 1',
+          image: 'тема 1',
+        },
+        {
+          title: 'Тема 2',
+          image: 'тема 2',
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Themes', null, {});
+  },
+};
