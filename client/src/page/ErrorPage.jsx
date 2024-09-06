ce77d77adce9f7bef16ecc57a08608c353e1a2ea
Polina-VA <polinavalhaeva3@gmail.com>
2024-09-06 14:36:11 +0300

    creatin questionPage

---
+++ client/src/page/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div>
+      <img src="https://www.shutterstock.com/image-vector/404-error-page-explorer-man-260nw-2161180329.jpg" alt="404" />
+    </div>
+  )
+}
+
+export default ErrorPage
--- client/src/page/QuestionPage.jsx
+++ client/src/page/QuestionPage.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import apiAxiosInstance from '../services/apiAxiosInstance';
 
-function QuestionPage() {
+function QuestionPage({ questions}) {
+   const { questionId} = useParams
+
+  const [questionsByThemeId, setQuestionsByThemeId] = useState([])
+
+   const [statusOfQuestion, setStatusOfQuestion] = useState(false);
+   const [answer, setAnswer] = useState("");
+   const navigate = useNavigate();
+  
+  
+console.log(questionsByThemeId);
+  //  const onHandleGetQuestion = async (questionId) => {
+  //   try {
+  //     const { data } = await apiAxiosInstance.get(`/questions/${questionId}`);
+
+  //     //setQuestions([...data.questions]);
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  const handleAnswerChange = (event) => {
+    event.preventDefault();
+    setAnswer(event.target.value);
+  };
+
+  useEffect(() => {
+    // setQuestionsByThemeId(questions.filter((que) => que.theme_id === themeId))
+  }, [])
+
   return (
-    <div>QuestionPage</div>
+    <div className='questionContainer'>
+     <img className="img" src={questionsByThemeId[questionId]?.image} />
+        <div>{questionsByThemeId[questionId]?.question}</div>
+        <input
+          onChange={handleAnswerChange}
+          className="input"
+          placeholder="Каков ваш ответ?"
+        />
+        <button
+          className="btn"
+          onClick={(event) => {
+            if (answer === questionsByThemeId[questionId]?.answer) {
+              event.preventDefault();
+              questionId += 1);
+              alert("правильно!");
+            } else {
+              alert("Ты ошибся");
+            }
+          }}
+        >
+          Проверить
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            if (currentQuestion < questionsByThemeId.length - 1) {
+              setCurrentQuestion((prev) => prev + 1);
+            } else {
+              navigate("/Menu");
+            }
+          }}
+
+        >
+          Вперед
+        </button>
+    </div>
   )
 }
 
